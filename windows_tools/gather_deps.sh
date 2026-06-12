44cfb05f82841470abd3cec03d80d3c9593c7c8a
The Marlboro Man <marlborometal@gmail.com>
2026-06-12 08:49:37 +0200

    ...

--- windows_tools/gather_deps.sh
+++ windows_tools/gather_deps.sh
@@ -20,5 +20,34 @@ git clone https://github.com/themarlboroman/ldtools
 git clone https://github.com/themarlboroman/appconsole
 git clone https://github.com/themarlboroman/dfw
 
+cat > "${1}/build_all.sh" <<- EOM
+#!/bin/bash
+
+function build_project {
+
+	cd $1
+
+	branch=$(git branch --show-current)
+	git reset --hard HEAD
+	git pull origin $branch
+	rm -rf build
+	mkdir build
+	cd build
+	cmake .. -G "MSYS Makefile" -DBUILD_STATIC=ON -DBUILD_SHARED=ON -DBUILD_DEBUG=ON
+	make
+}
+
+build_project log
+build_project tools
+build_project appenv
+build_project console
+build_project libdansdl2
+build_project ldtools
+build_project appconsole
+build_project dfw
+EOM
+
+chmod +x ${1}/build_all.sh
+
 echo "now you can go into ${1} and run ./build_all.sh to make all libs"
 
