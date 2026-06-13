0fcf1933449ecfccf97bd8e1de037058f7784f05
The Marlboro Man <marlborometal@gmail.com>
2026-06-13 02:42:51 +0200

    ...

--- windows_tools/gather_windows_libs.sh
+++ windows_tools/gather_windows_libs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "install ntldd and do ntldd -R executable.file | grep -v "ms-ext" | grep "not found" and fix those";
+
+
 if [ $# -ne 1 ]
 then 
 
@@ -9,18 +12,20 @@ fi
 
 cd $1
 
+º
 echo "copying all libs from local builds..."
 cp ../../*/build/*.dll ./
 
-echo "running ldd to find the rest..."
-ldd the-dreaming-tower.exe | awk {'print $1'} > libs.txt
-for file in $(cat libs.txt)
-do
+#THIS BREAKS THE BUILD, I TELL YOU!
+#echo "running ldd to find the rest..."
+#ldd the-dreaming-tower.exe | awk {'print $1'} > libs.txt
+#for file in $(cat libs.txt)
+#do
 
-	location="/c/msys64/mingw64/bin/$file"
+#	location="/c/msys64/mingw64/bin/$file"
 
-	if [ -f $location ]
-	then
-		cp $location ./
-	fi
-done
+#	if [ -f $location ]
+#	then
+#		cp $location ./
+#	fi
+#done
